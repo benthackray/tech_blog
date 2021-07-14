afc588586916f8ccac03d79302c284b8a21583d2
Ben Thackray <mthack92@gmail.com>
2021-07-14 19:56:59 -0400

    no idea why my data attribute is not working

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,22 @@
 const commentFormHandler = async (event) => {
     event.preventDefault();
-    const text = document.querySelector('#comment-text').value.trim();
+    const text = await document.querySelector('#comment-text').value.trim();
     const post_id = text.dataset.post;
     if (text && post_id) {
-      const response = await fetch('/api/comments/', {
-        method: 'POST',
-        body: JSON.stringify({ text, post_id }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to submit post');
-      }
+        const response = await fetch('/api/comments/', {
+            method: 'POST',
+            body: JSON.stringify({ text, post_id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/post');
+        } else {
+            alert('Failed to submit post');
+        }
     }
-  };
-  
-  document
+};
+
+document
     .querySelector('.comment-form')
     .addEventListener('submit', commentFormHandler);
